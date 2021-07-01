70891b6efa71090f8e530dac9b915ce51f62ae91
jiyuha <momos1ok@naver.com>
2021-07-01 14:57:11 +0900

    제발

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from chefboost import Chefboost as cb
+
+# ----------------------------------------------
+
+parallelism_cases = [True]
+# parallelism_cases = [False]
+# parallelism_cases = [False, True]
+
+if __name__ == '__main__':
+
+    for enableParallelism in parallelism_cases:
+        print("-------------------------")
+
+        print("Validation set case")
+
+        df = pd.read_csv("dataset/wine.csv")
+        validation_df = pd.read_csv("dataset/wine.csv")
+        config = {'algorithm': 'C4.5', 'enableParallelism': enableParallelism}
+        model = cb.fit(df, config, validation_df=validation_df)
+
+    print("-------------------------")
+    print("unit tests completed successfully...")
